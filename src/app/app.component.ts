09a4281417db1715f7195cc504196a200cea7d42
Эмиль <vagab@>
2023-04-06 16:01:18 +0300

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ap';
+  counter = 0
+  updatedAt?:number
+  get cannotDecrease():boolean{
+    return this.counter <=0;
+  }
+  increase():void{
+    this.updatedAt = Date.now()
+    this.counter++
+  }
+  decrease():void{
+    this.updatedAt = Date.now()
+    this.counter--
+  }
+  reset():void {
+    this.updatedAt = Date.now()
+    this.counter = 0
+  }
 }
